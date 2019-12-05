8a86c480ba79617a390ffa38e24a46d8de68ca73
Alex-Moreno98 <58548062+Alex-Moreno98@users.noreply.github.com>
2019-12-05 10:03:35 +0100

    Create _2019-10-23_Practica3_Alejandro.ino

---
+++ code/_2019-10-23_Practica3_Alejandro.ino
@@ -0,0 +1,34 @@
+
+//Practica 3
+
+void setup() 
+{
+  pinMode(3, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop() 
+{
+  allON();
+  delay(1000);
+  allOFF();
+  delay(500);                       
+}
+
+void allON()
+{
+  digitalWrite(3,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(A3,HIGH);
+  digitalWrite(9,HIGH);    
+}
+
+void allOFF()
+{
+  digitalWrite(3,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(A3,LOW);
+  digitalWrite(9,LOW);    
+}
